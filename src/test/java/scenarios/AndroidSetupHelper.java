29f37fc2f3dac67df308f95a3ecf8f31004e6f75
nrublevsky <nickrublevsky@gmail.com>
2019-02-13 00:10:04 +0200

    Sup ma niBBas

---
+++ src/test/java/scenarios/AndroidSetupHelper.java
@@ -0,0 +1,56 @@
+package scenarios;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidSetupHelper {
+
+    public AndroidDriver initSetup() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Nexus 6");
+        caps.setCapability("udid", "emulator-5554");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "8.0");
+        caps.setCapability("appPackage", "com.rozdoum.eventor.lpmd2018");
+        caps.setCapability("appActivity", "com.rozdoum.eventor.activities.SchedulerActivity");
+//        caps.setCapability("noReset", "true");
+
+
+        URL url = new URL("http://0.0.0.0:4723/wd/hub");
+        AndroidDriver driver = new AndroidDriver(url, caps);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        String SyncMsgExp = "Synchronisation with server, please wait";
+        String SyncMsgAct = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v7.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.TextView")).getText();
+
+
+        driver.launchApp();
+        System.out.println("This is routine before each test");
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v7.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.TextView"))));
+//        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v7.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.TextView"));
+        if (SyncMsgExp.equals(SyncMsgAct)) {
+            System.out.println("Synchronisation is going good");
+        } else {
+            System.out.println("Ooops, Synchronization isn't going anywhere");
+        }
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("com.rozdoum.eventor.lpmd2018:id/alertTitle")));
+        System.out.println("Ok, and now to the Tests");
+
+        return driver;
+    }
+
+    public  AndroidDriver tearDown() throws MalformedURLException {
+        AndroidDriver driver = MasterTest.driver;
+
+        System.out.println("This is routine after each test");
+        System.out.println("Closing app");
+        driver.closeApp();
+        return driver;
+    }
+}
---
+++ src/test/java/scenarios/MasterTest.java
@@ -0,0 +1,60 @@
+package scenarios;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MasterTest {
+
+    public static AndroidDriver driver;
+
+    @Before
+    public void init() throws MalformedURLException {
+        driver = new AndroidSetupHelper().initSetup();
+    }
+
+
+    @Test
+    public void TCase() throws MalformedURLException, InterruptedException, NullPointerException {
+
+
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        String EventNameExp = "Atlassian Summit | San Jose, CA";
+
+        System.out.println("Hola! Starting a Name Check Test");
+        System.out.println("Waiting till Menu button becomes active" + "Waiting for 60 sec (because this PC is slooooooooow)");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]")));
+//        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout")).isDisplayed();
+        System.out.println("Clicking Menu button");
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]")).click();
+        System.out.println("Obtaining Event Name");
+        String EventNameAct = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.TextView")).getText();
+
+        System.out.println("Comparing Expected and Actual Names");
+        if (EventNameAct.equals(EventNameExp)) {
+            System.out.println("Yep, names is correct " +
+                    EventNameAct + " = " + EventNameExp);
+        } else {
+            System.out.println("No, something is wrong here: " +
+                    EventNameAct + " != " + EventNameExp);
+        }
+        System.out.println("This was last step of the test");
+    }
+
+    @Test
+
+
+    @After
+    public void tear() throws MalformedURLException {
+        driver = new AndroidSetupHelper().tearDown();
+    }
+
+}
